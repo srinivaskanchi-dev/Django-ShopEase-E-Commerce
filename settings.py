2a2d170f7a968921169e2987eb2176e129d44b45
srinivaskanchi-dev <srinivaskanchi752@gmail.com>
2025-03-05 01:05:06 +0530

    added gitignore

--- settings.py
+++ settings.py
@@ -1,10 +1,34 @@
-# Email settings
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Secret Key Configuration
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+# Database Configuration
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
+
+# Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # Or your email provider's SMTP server
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shopease.ecomweb@gmail.com'
-EMAIL_HOST_PASSWORD = 'tugc sdmr ycco skym'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
 
 SITE_NAME = 'ShopEase'
 
